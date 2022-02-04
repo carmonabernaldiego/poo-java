89a451771b68161efd8dcde5858aabdacd05615e
CBDX <cbdiegox@gmail.com>
2022-02-03 20:43:44 -0600

    update

---
+++ EstructuraDatos/miClase/Main.java
@@ -0,0 +1,12 @@
+package miClase;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        double n1 = 10, n2 = 6, resultado;
+
+        resultado = n1 + n2;
+
+        System.out.println(resultado);
+    }
+}
