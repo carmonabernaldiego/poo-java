7c2084bbef003000c6e6487f437589954cbfe2cc
CBDX <cbdiegox@gmail.com>
2022-02-11 23:14:37 -0600

    update

--- POO/reproductor/Reproductor.java
+++ POO/reproductor/Reproductor.java
@@ -18,6 +18,18 @@ public class Reproductor {
                 "Fear Inoculum [Explicit]", "Tool Dissectional, L.L.C./Volcano Entertainment II, L.L.C.."));
         listaMusic.add(new Musica("Butter", "BTS", Formato.mp3, 2.44, 2021,
                 "Butter", "BIGHIT MUSIC / HYBE"));
+        listaMusic.add(new Musica("Cyber Sex [Explicit]", "Doja Cat", Formato.mp3, 2.45, 2019,
+                "HOT PINK [EXPLICIT]", "Kemosabe Records/RCA Records"));
+        listaMusic.add(new Musica("King James", "Buckethead", Formato.mp3, 3.57, 2009,
+                "CRIME SLUNK SCENE", "Buckethead"));
+        listaMusic.add(new Musica("Sober [Explicit]", "TOOL", Formato.mp3, 5.06, 1993,
+                "UNDERTOW [EXPLICIT]", "Tool Dissectional, L.L.C/Volcano Entertainment II, L.L.C.."));
+        listaMusic.add(new Musica("Breathe (In the Air)", "Pink Floyd", Formato.mp3, 2.49, 1973,
+                "The Dark Side of the Moon", "The copyright in this sound recording is owned by Pink Floyd Music Ltd., marketed and distributed by Sony Music Entertainment"));
+        listaMusic.add(new Musica("Any Colour You Like", "Pink Floyd", Formato.mp3, 3.25, 1973,
+                "The Dark Side of the Moon", "The copyright in this sound recording is owned by Pink Floyd Music Ltd., marketed and distributed by Sony Music Entertainment"));
+        listaMusic.add(new Musica("Breaking the Law", "Judas Priest", Formato.mp3, 2.35, 2010,
+                "BRITISH STEEL - 30TH ANNIVERSARY", "Sony Music Entertainment UK Limited"));
 
         System.out.println(listaMusic.toStringMusic());
     }
