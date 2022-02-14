7aee6fba35d02a06e5f05deb2f7973cbbd40c4a2
CBDX <cbdiegox@gmail.com>
2022-02-14 10:41:48 -0600

    update

---
+++ POO/ReproductorGrafico/src/reproductorGrafico/ReproductorGrafico.java
@@ -0,0 +1,8 @@
+package reproductorGrafico;
+
+public class ReproductorGrafico {
+    public static void main(String[] args){
+         MenuPrincipal menu = new MenuPrincipal();
+         menu.setVisible(true);
+    } 
+}
